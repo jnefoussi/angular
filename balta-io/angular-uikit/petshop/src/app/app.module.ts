38280bc1f0cc60fa85e9c3b204855a107f1807a3
Jonathan J Nefoussi <jnefoussi@gmail.com>
2022-08-09 08:14:24 -0300

    Organizando o projeto

--- balta-io/angular-uikit/petshop/src/app/app.module.ts
+++ balta-io/angular-uikit/petshop/src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { ResetPasswordPageComponent } from './reset-password-page/reset-password-page.component';
-import { SignupPageComponent } from './signup-page/signup-page.component';
-import { RouterModule } from '@angular/router';
-import { PetsPageComponent } from './pets-page/pets-page.component';
-import { ProductsPageComponent } from './products-page/products-page.component';
-import { CartPageComponent } from './cart-page/cart-page.component';
+import { LoginPageComponent } from './pages/account/login-page/login-page.component';
+import { PetsPageComponent } from './pages/account/pets-page/pets-page.component';
+import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
+import { SignupPageComponent } from './pages/account/signup-page/signup-page.component';
+import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
+import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
 
 @NgModule({
   declarations: [
